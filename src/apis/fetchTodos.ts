8e0c1075ec5bbb4253c7c25615bb188953e8f443
shumo.tokinaga <longtime1116@gmail.com>
2018-08-23 14:17:32 +0900

    [React] Fetch todo data with jsonplaceholder and axios

--- src/apis/fetchTodos.ts
+++ src/apis/fetchTodos.ts
@@ -1,17 +1,4 @@
-import { Todo } from '../types';
-/**
- * Mock データを返す
- * Promise.resolve() に渡された値が then の response に入る
- * @example
- * fetchTodos().then(response => {
- *   console.log(response); // { data: [...] }
- *   console.log(response.data[0]); // { userId: 1, id: 1, title: 'foo', ... }
- * });
- */
-export const fetchTodos = () => {
-  const data: Todo[] = [
-    { userId: 1, id: 1, title: 'foo', completed: false },
-    { userId: 1, id: 2, title: 'bar', completed: true }
-  ];
-  return Promise.resolve({ data });
-};
+import axios from 'axios';
+
+export const fetchTodos = () =>
+  axios.get("https://jsonplaceholder.typicode.com/todos?_limit=3'");
