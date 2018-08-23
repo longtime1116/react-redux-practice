3f43faabce3a4351f83f073aa680eef846c1afca
shumo.tokinaga <longtime1116@gmail.com>
2018-08-23 10:16:55 +0900

    [React] Show todos

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,8 @@
 import * as React from 'react';
+import TodosContainer from './components/TodosContainer';
 
-export const App = () => <div>Hello, world!</div>;
+export const App = () => (
+  <div>
+    <TodosContainer />
+  </div>
+);
