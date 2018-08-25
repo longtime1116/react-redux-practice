22822eebc2faa7d946ca5eaac0ac026c0d3c5141
longtime1116 <longtime1116@gmail.com>
2018-08-25 15:56:47 +0900

    [Redux] Use selector to get state from redux store

--- src/containers/TodosContainer.tsx
+++ src/containers/TodosContainer.tsx
@@ -3,6 +3,7 @@ import { connect } from 'react-redux';
 import { RootState } from '../reducers/rootReducer';
 import { Todo } from '../types/CommonTypes';
 import { Todos } from './Todos';
+import { getTodos } from '../selectors/getTodos';
 
 export interface Props {
   todos: Todo[];
@@ -13,7 +14,7 @@ export interface Props {
 export interface State {}
 
 const mapStateToProps = (state: RootState) => {
-  const { todos, loading } = state.todos;
+  const { todos, loading } = getTodos(state);
   return {
     todos,
     loading
