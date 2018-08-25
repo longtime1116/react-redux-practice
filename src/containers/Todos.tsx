50d3d1cbd5fcc1d273aa9e94e977a2c4f263a2ce
longtime1116 <longtime1116@gmail.com>
2018-08-25 15:43:02 +0900

    [Redux] Extract Todos component

---
+++ src/containers/Todos.tsx
@@ -0,0 +1,42 @@
+import * as React from 'react';
+import { ControlPanel } from '../components/ControlPanel';
+import { TodoList } from '../components/TodoList';
+import { Props, State } from './TodosContainer';
+
+export class Todos extends React.Component<Props, State> {
+  private todoList: TodoList | null = null;
+
+  public componentDidMount() {
+    const { fetchTodos } = this.props;
+    fetchTodos();
+  }
+
+  public render() {
+    const { todos, loading } = this.props;
+
+    if (loading) {
+      return <div>loading...</div>;
+    }
+    return (
+      <React.Fragment>
+        <TodoList
+          todos={todos}
+          onAddTodo={this.handleAddTodo}
+          ref={todoList => (this.todoList = todoList)}
+        />
+        <ControlPanel onClick={this.handleClickFocusButton} />
+      </React.Fragment>
+    );
+  }
+
+  private handleAddTodo = (title: string) => {
+    const { addTodo } = this.props;
+    addTodo(title);
+  };
+
+  private handleClickFocusButton = () => {
+    if (this.todoList) {
+      this.todoList.focus();
+    }
+  };
+}
--- src/containers/TodosContainer.tsx
+++ src/containers/TodosContainer.tsx
@@ -1,56 +1,16 @@
-import * as React from 'react';
 import * as actions from '../actions';
 import { connect } from 'react-redux';
-import { ControlPanel } from '../components/ControlPanel';
-import { TodoList } from '../components/TodoList';
 import { RootState } from '../reducers/rootReducer';
 import { Todo } from '../types/CommonTypes';
+import { Todos } from './Todos';
 
-interface Props {
+export interface Props {
   todos: Todo[];
   loading: boolean;
   addTodo: Function;
   fetchTodos: Function;
 }
-interface State {}
-
-class Todos extends React.Component<Props, State> {
-  private todoList: TodoList | null = null;
-
-  public componentDidMount() {
-    const { fetchTodos } = this.props;
-    fetchTodos();
-  }
-
-  public render() {
-    const { todos, loading } = this.props;
-
-    if (loading) {
-      return <div>loading...</div>;
-    }
-    return (
-      <React.Fragment>
-        <TodoList
-          todos={todos}
-          onAddTodo={this.handleAddTodo}
-          ref={todoList => (this.todoList = todoList)}
-        />
-        <ControlPanel onClick={this.handleClickFocusButton} />
-      </React.Fragment>
-    );
-  }
-
-  private handleAddTodo = (title: string) => {
-    const { addTodo } = this.props;
-    addTodo(title);
-  };
-
-  private handleClickFocusButton = () => {
-    if (this.todoList) {
-      this.todoList.focus();
-    }
-  };
-}
+export interface State {}
 
 const mapStateToProps = (state: RootState) => {
   const { todos, loading } = state.todos;
